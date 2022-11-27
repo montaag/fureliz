183e7ccc1dca0b7409d44af770e31acb3964fada
Selin <34759967+montaag@users.noreply.github.com>
2022-11-27 17:58:48 +0300

    reward balance

--- lib/dataProvider/reward_provider.dart
+++ lib/dataProvider/reward_provider.dart
@@ -4,13 +4,13 @@ import 'package:yeliz/models/reward.dart';
 class RewardProvider {
   static List<Reward> getRewards() {
     List<Reward> rewards = [
-      Reward(reward: RewardModel(title: "İskender Döner", amount: 500), icon: FontAwesomeIcons.bowlFood),
-      Reward(reward: RewardModel(title: "Kitap", amount: 150), icon: FontAwesomeIcons.book),
-      Reward(reward: RewardModel(title: "Nakit\n20 lira", amount: 70), icon: FontAwesomeIcons.moneyBill),
-      Reward(reward: RewardModel(title: "Nakit\n50 lira", amount: 300), icon: FontAwesomeIcons.moneyBill1Wave),
-      Reward(reward: RewardModel(title: "Nakit\n100 lira", amount: 550), icon: FontAwesomeIcons.moneyBillTrendUp),
-      Reward(reward: RewardModel(title: "Kahve", amount: 60), icon: FontAwesomeIcons.coffee),
-      Reward(reward: RewardModel(title: "İstanbul Bileti x2", amount: 10000), icon: FontAwesomeIcons.solidPaperPlane),
+      Reward(reward: RewardModel(title: "İskender Döner", amount: 2500), icon: FontAwesomeIcons.bowlFood),
+      Reward(reward: RewardModel(title: "Kitap", amount: 900), icon: FontAwesomeIcons.book),
+      Reward(reward: RewardModel(title: "Nakit\n20 lira", amount: 400), icon: FontAwesomeIcons.moneyBill),
+      Reward(reward: RewardModel(title: "Nakit\n50 lira", amount: 1000), icon: FontAwesomeIcons.moneyBill1Wave),
+      Reward(reward: RewardModel(title: "Nakit\n100 lira", amount: 2500), icon: FontAwesomeIcons.moneyBillTrendUp),
+      Reward(reward: RewardModel(title: "Kahve", amount: 350), icon: FontAwesomeIcons.coffee),
+      Reward(reward: RewardModel(title: "İstanbul Bileti x2", amount: 1000000), icon: FontAwesomeIcons.solidPaperPlane),
     ];
     return rewards;
   }
